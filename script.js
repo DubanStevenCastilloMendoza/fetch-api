fde55640f7d6038cb5da89afebd20dccbe925263
DubanStevenCastilloMendoza <estivencastmen@gmail.com>
2023-04-28 07:11:02 +0200

    rick and morty

---
+++ script.js
@@ -0,0 +1,21 @@
+let url=  "https://rickandmortyapi.com/api/character/2"
+
+//consumir API
+fetch(url)
+.then((data) =>{
+    data.json()
+    .then((json)=>{
+        console.log(json)
+        alert(json.name)
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+})
+.catch((error)=>{
+    console.log(error)
+})
+
+
+//pasar el response a json 
+
